6d75850fdc1003e1a863e8883702d801d046e813
Ratul Krisna <ratul.krisna01@protonmail.com>
2022-12-15 20:08:41 +0530

    coyote time

--- src/en/Player.hx
+++ src/en/Player.hx
@@ -8,6 +8,7 @@ class Player extends Entity {
   public var camera:Rl.Camera2D;
 
   public var is_just_on_floor = false;
+  public var coyote_timer = 0.2;
 
   public function new(x:Float, y:Float, f:String, i:Input, l:ldtk.Layer_Tiles) {
     super(x, y, l);
@@ -24,22 +25,36 @@ class Player extends Entity {
   }
 
   override function update() {
+    if(coyote_timer != 0 && !is_on_floor) {
+      coyote_timer -= 0.016;
+    }
+
+    if(coyote_timer < 0) {
+      coyote_timer = 0;
+    }
+
+    if(is_on_floor) {
+      coyote_timer = 0.2;
+    }
+
+    trace(coyote_timer);
+
     // idle animation
     if(velocity_x == 0 && velocity_y == 0) {
     }
 
     // Movement
     if(Rl.isKeyDown(input.LEFT)) {
-      velocity_x = -0.25;
+      velocity_x = -0.18;
       texture_width = -13;
     }
     if(Rl.isKeyDown(input.RIGHT)) {
-      velocity_x = 0.25;
+      velocity_x = 0.18;
       texture_width = 13;
     }
 
     // jump
-    if(is_on_floor && Rl.isKeyPressed(input.JUMP)) {
+    if(is_on_floor && Rl.isKeyPressed(input.JUMP) || coyote_timer != 0 && Rl.isKeyPressed(input.JUMP)) {
       // jumping stretch
       setSquashX(0.5);
       velocity_y = -0.5;
